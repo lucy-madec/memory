83e3d6f54de65164ed5734151b9a132873aa6e4e
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-05-09 10:50:01 +0200

    Réinstallation de 'npm'

--- src/App.js
+++ src/App.js
@@ -2,25 +2,12 @@ import Button from './components/Button/Button';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button text="Je suis un bouton" />
     </div>
   );
-}
+};
 
 export default App;
