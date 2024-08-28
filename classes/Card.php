1a278523f55612c69b8f474ed97e98fa853d3d1f
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-08-28 13:56:11 +0200

    Adding te game card

---
+++ classes/Card.php
@@ -0,0 +1,13 @@
+<?php
+class Card {
+    public $id;
+    public $image;
+    public $pairId;
+
+    public function __construct($id, $image, $pairId) {
+        $this->id = $id;
+        $this->image = $image;
+        $this->pairId = $pairId;
+    }
+}
+?>
