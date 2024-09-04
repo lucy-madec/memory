749c1bf3cfa399fbc2232760a68bd29fb01da6bb
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-09-04 11:21:10 +0200

    Added a favicon

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Memory</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png" />
 </head>
 
 <body class="bg-light">
