333de2fe2d38e6754acb984f959774df19d1470d
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-09-04 16:00:59 +0200

    Changed the title name

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Memory</title>
+    <title>Creepy Memory</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="icon" type="image/png" href="assets/img/favicon.png" />
